db2c7f05d86edaea60cf919c2b79a15ddae0066f
unknown <sashardoval@gmail.com>
2023-01-17 15:26:26 -0800

    THE TIMER WORKS!

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var titleEl = $("<h2>");
 var paragraphEl = $("<p>");
-var startButton = $("<button>")
+var startButton = $("<button>");
 var timerCountdown = $("<li>");
 
 var alertEl = $("<p>");
@@ -17,7 +17,7 @@ paragraphEl.text("Try to answer all the questions as quickly and accurately as p
 paragraphEl.attr("class", "paragraph-element").addClass("format");
 //start button
 startButton.text("Start Quiz");
-startButton.attr("class", "start-button")
+startButton.attr("class", "start-button");
 startButton.addClass("format");
 
 
@@ -25,7 +25,25 @@ startButton.addClass("format");
 
 
 // list of all questions, choices, and answers
+var secondsLeft = 75;
+  
+  function setTime() {
 
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+       timeEl.text("Timer: " + secondsLeft);
+  
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+      }
+  
+    }, 1000);
+
+    startQuiz();
+
+   }
 
   
 //todo: create a function that defines how the quiz works
@@ -35,6 +53,9 @@ function startQuiz(){
   paragraphEl.hide();
   startButton.hide();
   //show timer amd first question
+  
+
+
   var questions = [
     {
       title: 'Commonly used data types DO NOT include:',
@@ -70,7 +91,7 @@ function startQuiz(){
     },
   ];
 
-  const title = document.getElementById('#main')
+ // const title = document.getElementById('#main')
   //TODO: create timer (reference activity 09)
   
     //TODO: create a for loop that cycles through above array each time user selects an answer
@@ -94,7 +115,7 @@ function endQuiz(
 ){
 
 }
-startButton.on('click', startQuiz);
+startButton.on('click', setTime);
 
 //append page elements
 mainEl.append(titleEl);
